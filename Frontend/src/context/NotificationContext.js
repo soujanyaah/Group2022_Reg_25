5944852a767e92aff9af9425b8f9c3d7e7882f77
IT19123400 <it19123400@my.sliit.lk>
2022-11-19 14:05:42 +0530

    Secure Software Development

--- Frontend/src/context/NotificationContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export const NotificationContext = createContext({
-	notify: false,
-	message: null,
-	error: false,
-	showNotification: () => {},
-	clearNotification: () => {}
-});
